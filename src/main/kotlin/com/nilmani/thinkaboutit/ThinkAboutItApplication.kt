00633071366f16bd0676897eb023ee02cc48fafe
iMac <imac@iMacs-iMac-3.local>
2021-10-18 16:15:24 +0530

    Banner changed success full

---
+++ src/main/kotlin/com/nilmani/thinkaboutit/ThinkAboutItApplication.kt
@@ -0,0 +1,11 @@
+package com.nilmani.thinkaboutit
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ThinkAboutItApplication
+
+fun main(args: Array<String>) {
+	runApplication<ThinkAboutItApplication>(*args)
+}
